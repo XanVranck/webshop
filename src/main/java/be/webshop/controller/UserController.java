d3d6d2109b7edff8be68545328d882e73ce26c6a
Xan Vranck <vranckaert.xan@gmail.com>
2018-01-04 20:51:14 +0100

    changed entry point for logging in - singin up

--- src/main/java/be/webshop/controller/UserController.java
+++ src/main/java/be/webshop/controller/UserController.java
@@ -3,19 +3,32 @@ package be.webshop.controller;
 import be.webshop.user.User;
 import be.webshop.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.transaction.Transactional;
+
 @RestController
-@RequestMapping("/user")
+@Transactional
 public class UserController {
     @Autowired
     private UserService userService;
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
     @PostMapping("/sign-up")
-    public void signUp(@RequestBody User user) {
-        userService.store(user);
+    @RequestMapping(produces = "application/json")
+    public void signUp(@RequestParam String username,
+                       @RequestParam String password) {
+        User realUser = new User(username, bCryptPasswordEncoder.encode(password));
+        userService.store(realUser);
+    }
+
+    @PostMapping("/login")
+    public void login() {
+        System.out.println("loggin in");
     }
 }
